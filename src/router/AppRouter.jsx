af52442c855ec6e1ac904e21b7d56f1f6e4c3f7a
Martin Gonzalez <gonzalez.martin.mag@gmail.com>
2023-03-19 11:21:19 -0300

    Adding reac-router-dom and creating AppRouter

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+
+      {/* Login and Register */}
+      <Route />
+
+      {/* Journal App */}
+      <Route />
+    </Routes>
+  )
+}
