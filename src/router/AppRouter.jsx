c370a37e4e1269bc3ef9c4ab935a3bceeed9e143
Martin Gonzalez <gonzalez.martin.mag@gmail.com>
2023-03-19 12:03:28 -0300

    Adding app router

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,14 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
+import { AuthRoutes } from '../auth/routes/AuthRoutes';
+import { JournalRoutes } from '../journal/routes/JournalRoutes';
 
 export const AppRouter = () => {
   return (
     <Routes>
 
       {/* Login and Register */}
-      <Route />
+      <Route path='/auth/*' element={ <AuthRoutes />} />
 
       {/* Journal App */}
-      <Route />
+      <Route path='/*' element={ <JournalRoutes />} />
     </Routes>
   )
 }
