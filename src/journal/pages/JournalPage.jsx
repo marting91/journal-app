2c9977b85d809bf1aabcd1b8f191ffe14f03505a
Martin Gonzalez <gonzalez.martin.mag@gmail.com>
2023-03-28 00:54:38 -0300

    Creating Journal Layout component

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,6 +1,10 @@
+import { Typography } from '@mui/material';
+import { JournalLayout } from '../layout/JournalLayout';
 
 export const JournalPage = () => {
   return (
-    <div>JournalPage</div>
+    <JournalLayout>
+      <Typography>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum unde quam ipsam officiis pariatur, fugiat dolore impedit accusamus ipsum corrupti! Cumque molestias blanditiis adipisci vero, illo qui voluptatibus ducimus ratione.</Typography>
+    </JournalLayout>
   )
 }
