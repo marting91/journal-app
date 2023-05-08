45b975f8d5337468100231ef52e65c28d25e8710
Martin Gonzalez <gonzalez.martin.mag@gmail.com>
2023-05-08 15:24:30 +0200

    Adding CheckingAuth component

--- src/theme/AppTheme.jsx
+++ src/theme/AppTheme.jsx
@@ -5,9 +5,9 @@ import { purpleTheme } from './purpleTheme';
 export const AppTheme = ({ children }) => {
   return (
     <ThemeProvider theme={ purpleTheme }>
-    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-    <CssBaseline />
-    { children }
-  </ThemeProvider>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      { children }
+    </ThemeProvider>
   )
 }
