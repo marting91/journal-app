308f399f1f522e83e16f38221b6c544cf740a873
Martin Gonzalez <gonzalez.martin.mag@gmail.com>
2023-05-18 16:06:17 +0200

    Loading notes in NoteView from sidebar

--- src/journal/components/SidebarItem.jsx
+++ src/journal/components/SidebarItem.jsx
@@ -1,16 +1,25 @@
-import { TurnedInNot } from "@mui/icons-material"
-import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material"
-import { useMemo } from "react";
+import { useMemo } from 'react';
+import { useDispatch } from 'react-redux';
+import { TurnedInNot } from '@mui/icons-material'
+import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
 
-export const SidebarItem = ({ title, body }) => {
+import { setActiveNote } from '../../store/journal';
+
+export const SidebarItem = ({ title, body, id, date, imageUrls =[] }) => {
 
   const newTitle = useMemo( () => {
     return title.length > 17 ? title.substring(0,17) + '...' : title;
   }, [ title ] );
 
+  const dispatch = useDispatch();
+
+  const handleListItemClick = () => {
+    dispatch( setActiveNote({ title, body, id, date, imageUrls }) );
+  }
+
   return (
     <ListItem disablePadding>
-      <ListItemButton>
+      <ListItemButton onClick={ handleListItemClick }>
         <ListItemIcon>
           <TurnedInNot />
         </ListItemIcon>
--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,8 +1,21 @@
 import { SaveOutlined } from "@mui/icons-material"
 import { Button, Grid, TextField, Typography } from "@mui/material"
 import { ImageGallery } from "../components/ImageGallery"
+import { useForm } from "../../hooks/useForm"
+import { useSelector } from "react-redux"
+import { useMemo } from "react"
 
 export const NoteView = () => {
+
+  const { active: activeNote } = useSelector( state => state.journal );
+  const { body, title, date, onInputChange, formState } = useForm( activeNote );
+
+  const dateString = useMemo(() => {
+    const newDate = new Date( date );
+
+    return newDate.toUTCString();
+  }, [date]);
+
   return (
     <Grid 
       container 
@@ -12,7 +25,7 @@ export const NoteView = () => {
       className='animate__animated animate__fadeIn animate__faster'
     >
       <Grid item>
-        <Typography fontSize={ 39 } fontWeight='light'>August 29, 2023</Typography>
+        <Typography fontSize={ 39 } fontWeight='light'>{ dateString }</Typography>
       </Grid>
       <Grid item>
         <Button color="primary" sx={{ padding: 2 }}>
@@ -29,6 +42,9 @@ export const NoteView = () => {
           placeholder="Enter a title"
           label="Title"
           sx={{ border: 'none', mb: 1 }}
+          name="title"
+          value={ title }
+          onChange={ onInputChange }
         />
 
         <TextField
@@ -38,6 +54,9 @@ export const NoteView = () => {
           multiline
           placeholder="What happened today"
           minRows={ 5 }
+          name="body"
+          value={ body }
+          onChange={ onInputChange }
         />
       </Grid>
 
