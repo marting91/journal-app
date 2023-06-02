00ed9534af55169c833a683ea667c26aed0c967e
Martin Gonzalez <gonzalez.martin.mag@gmail.com>
2023-06-03 01:18:09 +0200

    Installing and configuring JEST

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -7,12 +7,12 @@ import { getFirestore } from 'firebase/firestore/lite';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCuKCn414oY1q7TDng29Fi5bX9OiX_OIHI",
-  authDomain: "react-journal-app-eb946.firebaseapp.com",
-  projectId: "react-journal-app-eb946",
-  storageBucket: "react-journal-app-eb946.appspot.com",
-  messagingSenderId: "442046815866",
-  appId: "1:442046815866:web:8c0b96e5c882c3318925c3"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
--- src/helpers/fileUpload.js
+++ src/helpers/fileUpload.js
@@ -1,6 +1,6 @@
 
 export const fileUpload = async ( file ) => {
-  const cloudURL = 'https://api.cloudinary.com/v1_1/dlon8gwl1/upload';
+  const cloudURL = import.meta.env.VITE_CLOUDINARY_URL;
 
   const formData = new FormData();
   formData.append('upload_preset', 'journal-app-react');
@@ -15,7 +15,7 @@ export const fileUpload = async ( file ) => {
     if ( !resp.ok ) throw new Error( 'Problem uploading iamge' );
 
     const cloudResp = await resp.json();
-    console.log({ cloudResp });
+
     return cloudResp.secure_url;
   } catch (error) {
     throw new Error( error.message );
