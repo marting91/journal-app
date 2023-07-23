6528c9aa031751bbcaded1d78ef34fb3a290f178
Martin Gonzalez <gonzalez.martin.mag@gmail.com>
2023-07-23 14:23:16 +0200

    Adding configuration to use env variables in JEST

--- babel.config.js
+++ babel.config.js
@@ -3,4 +3,16 @@ module.exports = {
       [ '@babel/preset-env', { targets: { esmodules: true } } ],
       [ '@babel/preset-react', { runtime: 'automatic' } ],
   ],
+  // For loading VITE_ environment variables in JEST
+  plugins: [
+    function() {
+      return {
+        visitor: {
+          MetaProperty(path) {
+            path.replaceWithSourceString('process')
+          }
+        }
+      }
+    }
+  ]
 };
--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,12 @@
 import 'whatwg-fetch';
+import 'setimmediate';
+import { config } from 'dotenv';
+
+// Load env variables in JEST
+config({
+  path: '.env.test',
+});
+
+// jest.mock('./src/helpers/getEnv', () => ({
+//   getEnv: () => ({ ...process.env }),
+// }));
